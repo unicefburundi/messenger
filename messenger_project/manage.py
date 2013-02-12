e4984e45bd7eb0d3626ed92b7bb51be76c4d25e3
Alfred Assey M <asseym@gmail.com>
2013-02-12 17:40:26 +0300

    clean up

--- messenger_project/manage.py
+++ messenger_project/manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-
-from django.core.management import execute_manager
-import settings
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
